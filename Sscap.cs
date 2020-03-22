29c23f25862d0419bb4cb3552b54c075489a3e01
becrevex <brent@cygienesolutions.com>
2020-03-22 12:52:49 -0700

    Added arguments, context, and logging to pentest mode.

--- Sscap.cs
+++ Sscap.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Drawing;
 using static System.Drawing.Graphics;
@@ -15,9 +16,10 @@ namespace ScreenShot
         // Main Method 
         static public void Main(String[] args) 
         { 
+            String[] techniques = new String[12] {"Initial Access", "Execution", "Persistence", "Privilege Escalation", "Defense Evasion",
+                        "Credential Access", "Discovery", "Lateral Movement", "Collection", "C2", "Exfiltration", "Impact"};
 
-            using (Bitmap bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width, 
-                                                        Screen.PrimaryScreen.Bounds.Height))
+            using (Bitmap bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height))
 
             using (Graphics g = Graphics.FromImage(bmpScreenCapture))
             {
@@ -26,11 +28,68 @@ namespace ScreenShot
                                 0, 0,
                                 bmpScreenCapture.Size,
                                 CopyPixelOperation.SourceCopy);
-                String stampedFileName = "ss" + DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss") + ".jpg";
-                bmpScreenCapture.Save(stampedFileName, ImageFormat.Jpeg);
+                String stampedFileName = "ss" + DateTime.Now.ToString("yyyyddM-HHmmss") + ".jpg";
+                if (args.Length == 1) {
+                    if (args[0] == "-p") {
+                        Console.Write("Tool: ");
+                        String ssTool = Console.ReadLine();
+                        Console.Write("Technique: ");
+                        String ssTechnique = Console.ReadLine();
+                        Console.Write("Note: ");
+                        String ssNote = Console.ReadLine();
+
+                        using (StreamWriter w = File.AppendText("pentest.txt")) {
+                            Log(ssNote, stampedFileName, ssTool, ssTechnique, w);
+                        }
+                        bmpScreenCapture.Save(stampedFileName, ImageFormat.Jpeg);
+                        Console.WriteLine("Screen shot captured.");
+                        Console.WriteLine("Pentest log entry created.");
+                        Console.WriteLine("Filename: " + stampedFileName.ToString());                        
+                    }
+                    if (args[0] == "-q") {
+                        bmpScreenCapture.Save(stampedFileName, ImageFormat.Jpeg);
+                        Console.WriteLine("Screen shot captured.");
+                        Console.WriteLine("Filename: " + stampedFileName.ToString()); 
+                    }
+                    if ((args[0] == "--interactive") || (args[0] == "-i")) {
+                        Console.WriteLine("Starting Sscap Interactive Mode.  Press ESC to quit.");
+                            while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
+                            {
+                                Console.WriteLine("");
+                                bmpScreenCapture.Save(stampedFileName, ImageFormat.Jpeg);
+                                Console.WriteLine("Screen shot captured.");
+                                Console.WriteLine("Filename: " + stampedFileName.ToString());
+                            }
+                        Console.WriteLine("Interactive mode finished.  Quitting.");
+                    }
+                } else {
+                    Console.WriteLine("\nScreen Shot Capture (Sscap) [Version 1.0.334]");
+                    Console.WriteLine("(c) 2020 Cygiene Solutions.  All rights reserved.");
+                    Console.WriteLine("");
+                    Console.WriteLine("Usage: Sscap.exe [-q] [-p] [-i|-interactive]");
+                    Console.WriteLine("     -q     Quick mode --takes a quick shot, no context.");
+                    Console.WriteLine("     -p     Pentest screen shot with tracking capabilities. Writes to log file.");
+                    Console.WriteLine("     -i     Interactive mode.  Waits for button press and snaps a shot.");
+
+                }
+
             }            
 
         } 
+
+        public static void Log(string ssNote, string fileName, string ssTool, string ssTechnique, TextWriter w)
+        {
+            w.Write("\r\nScreenshot Entry:    ");
+            w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+            w.WriteLine($"Filename:            {fileName}");
+            w.WriteLine($"Tool used:           {ssTool}");
+            w.WriteLine();
+            w.WriteLine($"Description:          \n {ssNote}");
+            
+            //w.WriteLine ("-------------------------------");
+        }
+
+
     }
 
 }
